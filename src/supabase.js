bd6a2f96eb0976f2700209f3399a9f19efea5693
hun-min <your@email.com>
2025-10-29 16:54:23 +0900

    새 Supabase 프로젝트로 변경

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://jpsvhbnjvwgjbrvsqckl.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impwc3ZoYm5qdndnamJydnNxY2tsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2OTUxNTksImV4cCI6MjA3NzI3MTE1OX0.jJ0PlS71Ig0-ZpNCHDH4iqn34uA_qdHFqfw38VoUwUs';
+const supabaseUrl = 'https://njywvoqiejiydvvmzhog.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qeXd2b3FpZWppeWR2dm16aG9nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE3MTQ5NDgsImV4cCI6MjA3NzI5MDk0OH0.waBdD4Ze7Sftohd7ks77cQx2-mQuoQsQqD25Md0ykEI';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
